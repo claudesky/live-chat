eac8d9c5e193a087e4b6a2aac402d356f838dd6c
Claude Kirke <claudekirke@gmail.com>
2021-04-05 00:18:10 +0900

    feat (front): create navbar component

---
+++ resources/js/components/navbar.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+nav.navbar.navbar-expand-lg.navbar-light.bg-white
+  a.navbar-brand(href='#') Navbar
+  .collapse.navbar-collapse.justify-content-end
+    .navbar-nav
+      router-link(
+        v-for="link in links"
+        :key="link.text"
+        :to="link.route"
+      ).nav-item.nav-link.mx-1.text-capitalize
+        | {{ link.text }}
+
+</template>
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          'text': 'home',
+          'route': {name: 'home'},
+        },
+        {
+          'text': 'login',
+          'route': {name: 'login'},
+        },
+        {
+          'text': 'register',
+          'route': {name: 'register'},
+        },
+      ]
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
