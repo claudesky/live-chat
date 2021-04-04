35d05a88fd775bf772516eac485392b1ae0120af
Claude Kirke <claudekirke@gmail.com>
2021-04-05 00:23:28 +0900

    feat (front): implement vue in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-require('./bootstrap');
+require('./bootstrap')
+
+import Vue from 'vue'
+
+// Vue Router
+import router from './routes'
+
+// Main layout
+import App from './main.vue'
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+    router,
+});
