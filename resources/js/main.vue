eb1800bf79ec78cb84ca64d52574baab846c0f35
Claude Kirke <claudekirke@gmail.com>
2021-04-05 00:22:43 +0900

    feat (front): create main layout

---
+++ resources/js/main.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+div.d-grid.basic-grid.main-container
+  navbar
+  main.container-fluid.bg-light
+    router-view
+  end-footer
+</template>
+<script>
+import navbar from './components/navbar'
+import endFooter from './components/end-footer'
+export default {
+  components: {
+    navbar,
+    'end-footer': endFooter,
+  },
+  data() {
+    return {
+    }
+  }
+};
+</script>
+<style scoped>
+.d-grid {
+  display: grid;
+}
+.basic-grid {
+  grid-template-columns: 100%;
+  grid-template-rows: 3rem auto 2.5rem;
+  grid-template-areas: 
+    "navbar"
+    "content"
+    "footer";
+}
+.main-container {
+  height: 100vh;
+}
+</style>
