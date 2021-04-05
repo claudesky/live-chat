e08709d2bb5a8588ceb0e480e4b8a4e3da898249
Claude Kirke <claudekirke@gmail.com>
2021-04-05 23:21:56 +0900

    feat (tests): create AuthTest

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    public function test_registration_works()
+    {
+        $uri = '/api/login';
+
+        $payload = [
+            'email' => 'test@example.org',
+            'name' => 'John Doe',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ];
+
+        $response = $this->json(
+            'POST',
+            $uri,
+            $payload,
+        );
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas(
+            'users',
+            [
+                'email' => $payload['email'],
+                'name' => $payload['name'],
+            ]
+        );
+    }
+}
