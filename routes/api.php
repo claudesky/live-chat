d84a8ea925deb7293e2e8e3a391292514c01a9e9
Claude Kirke <claudekirke@gmail.com>
2021-04-06 00:28:32 +0900

    feat (routes): change registration route endpoint to /register

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', [AuthController::class, 'login'])
-    ->name('login');
+Route::post('/register', [AuthController::class, 'login'])
+    ->name('register');
