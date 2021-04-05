68195dd26cb40dce030011bf7fb34e9fd03c698f
Claude Kirke <claudekirke@gmail.com>
2021-04-05 23:33:20 +0900

    feat (controllers): create AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        User::create(
+            $request->all()
+        );
+    }
+}
