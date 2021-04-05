a4ea716ef8f39770f21548460836714bf93b1506
Claude Kirke <claudekirke@gmail.com>
2021-04-06 01:06:12 +0900

    feat (controllers): add login method to Auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RegistrationRequest;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -12,5 +14,25 @@ class AuthController extends Controller
         User::create(
             $request->all()
         );
+    }
+
+    public function login(Request $request)
+    {
+        $credentials_match = Auth::attempt(
+            $request->only([
+                'email',
+                'password',
+            ])
+        );
+
+        if (!$credentials_match) {
+            return response('Invalid credentials', 401);
+        }
+
+        $user = User::where([
+            'email' => $request->input('email')
+        ])->first();
+
+        return $user;
     }
 }
