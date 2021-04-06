b573389bd7c9449721de909f2220ac908b6712f3
Claude Kirke <claudekirke@gmail.com>
2021-04-07 01:26:44 +0900

    feat (frontend): add basic login form

--- resources/js/pages/login.vue
+++ resources/js/pages/login.vue
@@ -1,10 +1,46 @@
 <template lang="pug">
-  div I am the login page
+.container
+  h1.text-center.my-5 Login
+  .card.my-5
+    .card-body
+      form(
+        @submit.prevent="submitLoginForm"
+      )
+        .form-group
+          label(for="email") Email address
+          input(
+            type="email"
+            name="email"
+            required
+            v-model="form_data.email"
+          )#email.form-control
+        .form-group
+          label(for="password") Password
+          input(
+            type="password"
+            name="password"
+            required
+            v-model="form_data.password"
+          )#password.form-control
+        button(type="submit").btn.btn-primary Login
 </template>
 <script>
 export default {
   data() {
     return {
+      form_data: {
+        email: '',
+        password: '',
+      }
+    }
+  },
+  methods: {
+    submitLoginForm: function() {
+      this.$store
+        .dispatch('self/login', this.form_data)
+        .then(response => {
+          console.log(response)
+        })
     }
   }
 }
