081597a528a8d113263a231476e4d738def7b0f0
Claude Kirke <claudekirke@gmail.com>
2021-04-06 23:47:50 +0900

    feat (routes): change /user endpoint to /self in api routes

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth']], function() {
 
-    Route::get('/user', function (Request $request) {
+    Route::get('/self', function (Request $request) {
         return $request->user();
     });
 
