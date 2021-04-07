72845df171133b8e224ab73a15b361f7357d266f
Claude Kirke <claudekirke@gmail.com>
2021-04-08 01:59:11 +0900

    feat (frontend): show sidebar and message-view components in dashboard page

--- resources/js/pages/dashboard.vue
+++ resources/js/pages/dashboard.vue
@@ -1,9 +1,16 @@
 <template lang="pug">
-.container.p-5.align-center
-  h1.text-center This is the dashboard
+.d-flex
+  sidebar.flex-grow-1
+  message-view.flex-grow-11
 </template>
 <script>
+import sidebar from '../components/sidebar'
+import messageView from '../components/message-view'
 export default {
+  components: {
+    'sidebar': sidebar,
+    'message-view' : messageView,
+  },
   data() {
     return {
     }
