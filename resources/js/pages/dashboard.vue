22d53209e23a508c8d663a4c8e9c12cfa91d5fde
Claude Kirke <claudekirke@gmail.com>
2021-04-08 00:45:18 +0900

    feat (frontend): create dashboard page

---
+++ resources/js/pages/dashboard.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+.container.p-5.align-center
+  h1.text-center This is the dashboard
+</template>
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
