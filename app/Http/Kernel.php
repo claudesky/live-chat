f2c3939d3c0bc9c3b53b1d7bbf6f504e27f19327
Claude Kirke <claudekirke@gmail.com>
2021-04-08 00:45:03 +0900

    feat (kernel): increase api rate limit to 120/min

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            'throttle:api',
+            'throttle:120,1',
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
