e0381c8f85c2381021af4b37b7fef093ec0ba294
Claude Kirke <claudekirke@gmail.com>
2021-04-08 01:58:18 +0900

    feat (frontend): create sidebar component

---
+++ resources/js/components/sidebar.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+.d-flex.flex-column
+  div I am a room
+  div I am a room
+  div I am a room
+  div I am a room
+  div I am a room
+  div I am a room
+  div I am a room
+</template>
