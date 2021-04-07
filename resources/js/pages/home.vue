de8261ba78d2ed4ee46ae121f56c68b37b737ca2
Claude Kirke <claudekirke@gmail.com>
2021-04-08 00:16:09 +0900

    feat (frontend): scaffold basic home page

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -1,5 +1,26 @@
 <template lang="pug">
-  div I am the home page
+.container.p-5.align-center
+  h1.text-center Welcome to Live Chat
+  p.text-center
+    | An app where anyone can register and start chatting with friends
+    br
+    | There are also rooms for you to join to hang out with your fam
+    br
+    br
+    router-link(
+      tag="a"
+      :to="{name: 'register'}"
+    ) Sign-up
+    |  now or 
+    router-link(
+      tag="a"
+      :to="{name: 'login'}"
+    ) login
+    |  if you already have an account
+  div.d-flex.justify-content-center
+    img.p-5.home-image(
+      src="/image/home_image.svg"
+    )
 </template>
 <script>
 export default {
@@ -9,3 +30,8 @@ export default {
   }
 }
 </script>
+<style scoped>
+.home-image {
+  height: 640px;
+}
+</style>
