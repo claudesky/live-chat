95a0d731a8d2e94f1d2983fbeb2a6a32b968e8fc
Claude Kirke <claudekirke@gmail.com>
2021-04-09 00:47:32 +0900

    feat (vue): create room-card component

---
+++ resources/js/components/room-card.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .room-card(
+    @click="openRoom"
+  ) I am a room
+</template>
+<script>
+export default {
+  methods: {
+    openRoom() {
+      this.$router.push({name: 'messages', params: {room_id: '123'}})
+    }
+  }
+}
+</script>
+<style scoped>
+.room-card {
+  cursor: pointer;
+}
+</style>
