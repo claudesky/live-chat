64adb4dcc15852107eda65bf28e4b7b03e48aaac
Claude Kirke <claudekirke@gmail.com>
2021-04-09 00:49:26 +0900

    feat (frontend): create new-request page

---
+++ resources/js/pages/dashboard/new-request.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+.d-flex.flex-column
+  div Do you want to send a new message request?
+  input(
+    placeholder="Type your message here"
+  )
+  button Send request
+</template>
