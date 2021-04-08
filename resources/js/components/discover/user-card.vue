804bfdf2e56cc712b32905dc40d89655ba8c0dcd
Claude Kirke <claudekirke@gmail.com>
2021-04-09 00:47:04 +0900

    feat (vue): create user-card component

---
+++ resources/js/components/discover/user-card.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+.d-flex-flex-column.user-card.card.p-3.m-3(
+  @click="startMessageRequest"
+)
+  img.rounded-circle(
+    src="/image/test-user.png"
+  )
+  h5.text-center.m-3 User
+</template>
+<script>
+export default {
+  methods: {
+    startMessageRequest() {
+      this.$router.push({name: 'new-request', params: {respondent_id: '123'}})
+    }
+  }
+}
+</script>
