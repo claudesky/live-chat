2bd7041b4f62c9a3e01f52628d4271c85215bdf2
Claude Kirke <claudekirke@gmail.com>
2021-04-09 00:49:11 +0900

    feat (frontend): create discover page

---
+++ resources/js/pages/dashboard/discover.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+.d-flex.flex-column.p-5
+  .card.border-0.main-card
+    .card-body.d-flex.flex-column
+      .card-title
+        h4 Invite someone to chat
+      div.d-inline-block.align-self-end
+        input(
+          placeholder="Search"
+        )
+        button Search
+      .d-flex.flex-wrap.justify-content-around.p-5
+        user-card(
+          v-for="i in 12"
+          :key="i"
+        )
+</template>
+<script>
+import userCard from '../../components/user-card'
+export default {
+  components: {
+    'user-card': userCard,
+  }
+}
+</script>
+<style>
+.main-card {
+  min-height: 20rem;
+}
+.user-card>img {
+  width: 7rem;
+}
+</style>
