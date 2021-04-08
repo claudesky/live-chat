9f2caeded02a22ce9eeedf9ff2ffe16483112390
Claude Kirke <claudekirke@gmail.com>
2021-04-09 00:50:10 +0900

    feat (vue): use room-card component in sidebar component

--- resources/js/components/sidebar.vue
+++ resources/js/components/sidebar.vue
@@ -1,10 +1,28 @@
 <template lang="pug">
 .d-flex.flex-column
-  div I am a room
-  div I am a room
-  div I am a room
-  div I am a room
-  div I am a room
-  div I am a room
-  div I am a room
+  div
+    form
+      input(
+        placeholder="search"
+      )
+      button(
+        type="submit"
+      ) Search
+  room-card(
+    v-for="i in 12"
+    :key="i"
+  )
+  div.mt-auto
+    router-link(
+      tag="button"
+      :to="{name: 'discover'}"
+    ) Discover
 </template>
+<script>
+import roomCard from '../components/room-card'
+export default {
+  components: {
+    'room-card': roomCard,
+  }
+}
+</script>
