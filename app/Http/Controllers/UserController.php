e136aa829a2579f5757d1539270eb1a72efbb264
Claude Kirke <claudekirke@gmail.com>
2021-04-13 06:07:12 +0900

    feat (controllers): return user in User/show controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,12 +32,12 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  User $user
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        //
+        return $user;
     }
 
     /**
