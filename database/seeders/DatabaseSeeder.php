7940d92626c0b74cc89bcbfebd71e1c7bf4c1925
Claude Kirke <claudekirke@gmail.com>
2021-04-13 02:37:15 +0900

    feat (seeders): add seeding of users to main database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Default test user
+        User::create([
+            'email' => 'test@example.org',
+            'name' => 'John Doe',
+            'password' => bcrypt('password'),
+        ]);
+
+        // Other users
+        User::factory(10)->create();
     }
 }
