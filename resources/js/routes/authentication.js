cf82702b57e120bb8055c58412ca09c06959d477
Claude Kirke <claudekirke@gmail.com>
2021-04-12 09:10:37 +0900

    feat (vue-router): add logout route with logout dispatch

--- resources/js/routes/authentication.js
+++ resources/js/routes/authentication.js
@@ -1,6 +1,9 @@
 import login from '../pages/login'
 import register from '../pages/register'
 
+// Vuex
+import store from '../store';
+
 export default [
     {
         path: '/login',
@@ -13,5 +16,17 @@ export default [
         name: 'register',
         component: register,
         meta: { guestOnly: true }
+    },
+    {
+        path: '/logout',
+        name: 'logout',
+        meta: { requiresAuth: true },
+        // When user goes to this link, log the user out.
+        beforeEnter: (to, from, next) => {
+            store.dispatch('self/logout')
+                .then(() => {
+                    next({ name: 'home' })
+                })
+        }
     },
 ]
